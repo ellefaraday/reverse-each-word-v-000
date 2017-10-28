54b00aadb5d6d34f5b00526eda8472985b0b439f
Elle Faraday <faraday.elle@gmail.com>
2017-10-28 17:47:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ def reverse_each_word(string)
   array = string.split(" ")
   array = array.collect {|word| word.reverse}
   string = array.join(" ")
-  puts string
 end
 
 reverse_each_word("hello margot")
